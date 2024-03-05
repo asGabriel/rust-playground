20f3faa9c36095ff2dab620c4380c7741e8d0267
asGabriel <andrade.gabriel2295@gmail.com>
2024-03-04 22:54:34 -0300

    feat: done exercise02

--- r_code_wars/src/main.rs
+++ r_code_wars/src/main.rs
@@ -1,6 +1,8 @@
 mod exercise_01;
+mod exercise_02;
 
 fn main() {
     println!("# Code Wars #");
     exercise_01::high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+    exercise_02::odd_or_even(vec![0, 1, 5]);
 }
